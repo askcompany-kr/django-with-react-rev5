998d6f90cb32280ba275d3b59e4559ec08a4b24f
Chinseok Lee <me@askcompany.kr>
2020-03-17 13:18:24 +0900

    14 - useAxios 훅을 통한 보다 손쉬운 조회 API 요청

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "antd";
 import Suggestion from "./Suggestion";
-import Axios from "axios";
+import useAxios from "axios-hooks";
 import { useAppContext } from "store";
 import "./SuggestionList.scss";
 
@@ -9,31 +9,27 @@ export default function SuggestionList({ style }) {
   const {
     store: { jwtToken }
   } = useAppContext();
-  const [userList, setUserList] = useState([]);
 
-  useEffect(() => {
-    async function fetchUserList() {
-      const apiUrl = "http://localhost:8000/accounts/suggestions/";
-      const headers = { Authorization: `JWT ${jwtToken}` };
-      try {
-        const { data } = await Axios.get(apiUrl, { headers });
-        setUserList(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUserList();
-  }, []);
+  const headers = { Authorization: `JWT ${jwtToken}` };
+
+  const [{ data: userList, loading, error }, refetch] = useAxios({
+    url: "http://localhost:8000/accounts/suggestions/",
+    headers
+  });
 
   return (
     <div style={style}>
+      {loading && <div>Loading ...</div>}
+      {error && <div>로딩 중에 에러가 발생했습니다.</div>}
+
       <Card title="Suggestions for you " size="small">
-        {userList.map(suggestionUser => (
-          <Suggestion
-            key={suggestionUser.username}
-            suggestionUser={suggestionUser}
-          />
-        ))}
+        {userList &&
+          userList.map(suggestionUser => (
+            <Suggestion
+              key={suggestionUser.username}
+              suggestionUser={suggestionUser}
+            />
+          ))}
       </Card>
     </div>
   );
