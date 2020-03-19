4175770d173d5b952988d4837847d74e9b5b04e2
Chinseok Lee <me@askcompany.kr>
2020-03-20 02:34:31 +0900

    실서비스용으로 장고 프로젝트를 도커로 빌드하기

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,5 +1,8 @@
 from .common import *
 
+DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
 DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
 
@@ -8,5 +11,15 @@ AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
 
 # 절대 이렇게 소스코드에 키를 넣지 마세요.
 # 아래 키는 예제로 넣었으며, 동작하지 않는 키입니다.
-# AZURE_ACCOUNT_NAME = "educastaskcompany"
-# AZURE_ACCOUNT_KEY = "nqWEdPMfsjFBoeU184HBnTsoaw4Tqyf0dWdhBtzM1k6jS9clFEHdxCoayvHhnI6LsNEdOC8erjBccyFKmHLGxQ=="
+# AZURE_ACCOUNT_NAME = ""
+# AZURE_ACCOUNT_KEY = ""
+
+# TODO: DATABASE
+# DATABASES = {}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"level": "ERROR", "class": "logging.StreamHandler",},},
+    "loggers": {"django": {"handlers": ["console"], "level": "ERROR",},},
+}
