011c23a92a4891073a7e4cbd5c0a2574a1b2129d
Chinseok Lee <me@askcompany.kr>
2020-03-20 04:30:09 +0900

    리액트 프로젝트를 Azure Storage에 배포하기

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -9,6 +9,8 @@ DEFAULT_FILE_STORAGE = "backend.storages.MediaAzureStorage"
 AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
 AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
 
+CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ORIGIN_WHITELIST", "").split(",")
+
 # 절대 이렇게 소스코드에 키를 넣지 마세요.
 # 아래 키는 예제로 넣었으며, 동작하지 않는 키입니다.
 # AZURE_ACCOUNT_NAME = ""
